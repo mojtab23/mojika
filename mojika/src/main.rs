872353f82b4a9f2f15d950ce4ced7304d41c750c
Mojtaba Zarezadeh <m.zarezadeh@inside.sahab.ir>
2023-02-04 19:15:53 +0330

    feat: Add Tracing

--- mojika/src/main.rs
+++ mojika/src/main.rs
@@ -1,21 +1,62 @@
 use std::error::Error;
 
+use tokio::select;
 use tokio::sync::broadcast;
+use tracing::debug;
+use tracing::Level;
 
 use mojika_discovery::Discovery;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let (shutdown_tx, shutdown_rx) = broadcast::channel(1);
+    setup_tracing()?;
+
+    let (shutdown_tx, mut shutdown_rx) = broadcast::channel(1);
 
     tokio::spawn(async move {
-        println!("watcher started");
+        debug!("watcher started");
         let _ = tokio::signal::ctrl_c().await;
         let r = shutdown_tx.send(());
-        println!("got ctrl+C {r:?}");
+        debug!("got ctrl+C {r:?}");
     });
 
-    let mut  discovery = Discovery::new(shutdown_rx).await?;
-    discovery.listen_multicast().await;
+    let discovery = Discovery::new().await?;
+
+    debug!("Start listening to multicast messages.");
+    loop {
+        select! {
+            msg = discovery.receive_new_message() => {
+                debug!("Got: {msg:?}");
+            }
+            res = shutdown_rx.recv() => {
+                debug!("Got {res:?} for shutdown");
+                break
+            }
+            else => {
+                debug!("Both channels closed");
+                break
+            }
+        }
+    }
+    Ok(())
+}
+
+fn setup_tracing() -> Result<(), Box<dyn Error>> {
+    // construct a subscriber that prints formatted traces to stdout
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        // Use a more compact, abbreviated log format
+        .compact()
+        // Display source code file paths
+        .with_file(true)
+        // Display source code line numbers
+        .with_line_number(true)
+        // Display the thread ID an event was recorded on
+        .with_thread_ids(true)
+        // Don't display the event's target (module path)
+        .with_target(false)
+        // Build the subscriber
+        .finish(); // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber)?;
     Ok(())
 }
--- mojika_discovery/src/lib.rs
+++ mojika_discovery/src/lib.rs
@@ -1,39 +1,26 @@
-use std::borrow::BorrowMut;
 use std::error::Error;
 use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 use tokio::net::UdpSocket;
-use tokio::select;
-use tokio::sync::broadcast::{self, Receiver, Sender};
+use tracing::debug;
 
 const DEFAULT_PORT: u16 = 10020;
 
 pub struct Discovery {
     socket: Arc<UdpSocket>,
-    messages_receiver: Receiver<String>,
-    messages_sender: Sender<String>,
-    shutdown_signal: Receiver<()>,
 }
 
 impl Discovery {
-    pub async fn new(shutdown_signal: Receiver<()>) -> Result<Self, Box<dyn Error>> {
-        let (sender, receiver) = broadcast::channel(32);
-
-        let socket = Self::create_socket().await?;
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let socket = Self::create_socket()?;
         let socket = Arc::new(socket);
-        let discovery = Self {
-            socket,
-            messages_receiver: receiver,
-            messages_sender: sender,
-            shutdown_signal,
-        };
-
+        let discovery = Self { socket };
         Ok(discovery)
     }
 
-    async fn create_socket() -> Result<UdpSocket, Box<dyn Error>> {
+    fn create_socket() -> Result<UdpSocket, Box<dyn Error>> {
         let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
         socket.set_reuse_address(true)?;
         socket.set_nonblocking(true)?;
@@ -47,40 +34,20 @@ impl Discovery {
         Ok(UdpSocket::from_std(std_socket)?)
     }
 
-    pub async fn listen_multicast(&mut self) {
-        println!("Start listening to multicast messages.");
-        loop {
-            select! {
-                msg = Self::receive_new_message(self.socket.clone()) => {
-                    println!("Got: {msg:?}");
-                    self.messages_sender.clone().send(msg);
-                }
-                res = self.shutdown_signal.recv() => {
-                    println!("Got {res:?} for shutdown");
-                    break
-                }
-                else => {
-                    println!("Both channels closed");
-                    break
-                }
-            }
-        }
-    }
-
-    async fn receive_new_message(socket: Arc<UdpSocket>) -> String {
+    pub async fn receive_new_message(&self) -> String {
+        let socket: Arc<UdpSocket> = self.socket.clone();
         let mut buf = vec![0u8; 1024];
         let result = socket.recv_from(&mut buf).await;
         match result {
             Ok((len, addr)) => {
                 let msg = String::from_utf8_lossy(&buf[..len]);
-                println!("message form address: {addr:?}");
+                debug!("message form address: {addr:?}");
                 msg.to_string()
             }
             Err(_) => "Error!".to_string(),
         }
     }
 }
-
 
 // #[cfg(test)]
 // mod tests {
